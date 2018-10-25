7280ec8db117a4fbe116f266a6b2a232ff78604a
niuniu <worldwildwu@outlook.com>
2018-10-25 22:11:35 +0800

    session保存位置指定和测试

--- manage.py
+++ manage.py
@@ -1,61 +1,56 @@
-# -*- coding: utf-8 -*-
-# @Author  : junjunzai
-# @Email   : junjunzai@163.com
-# @File    : manage.py
-# @Software: PyCharm
-
-from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
-from flask_wtf import CSRFProtect
+from flask import Flask, session
+from flask.ext.sqlalchemy import SQLAlchemy
+# 可以用来指定 session 保存的位置
 from flask_session import Session
-
-app = Flask(__name__)
 
 
 class Config(object):
-    """工程配置信息"""
-
+    """项目的配置"""
     DEBUG = True
 
-    # 配置数据库信息
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/information"
-    # 设置数据库跟踪
-    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
 
-    # 配置redis信息
-    REDIS_HOST = '127.0.0.1'
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Redis的配置
+    REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
-    # flask_session的配置信息
-    # 指定session保存到redis中
+    # Session保存配置
     SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True  # 让cookie中的session_id被加密处理
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis实例
-    PERMANENT_SESSION_LIFETIME = 86400
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
+app = Flask(__name__)
+# 加载配置
 app.config.from_object(Config)
+
+# 初始化数据库
 db = SQLAlchemy(app)
+# 初始化 redis 存储对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启当前项目 CSRF 保护，只做服务器验证功能
 CSRFProtect(app)
+# 设置session保存指定位置
 Session(app)
 
-# 创建中央管理者
-manager = Manager(app)
-
-# 将app与db关联
-Migrate(app, db)
-# 将迁移命令添加到manager中
-manager.add_command("db",MigrateCommand)
 
 @app.route('/')
 def index():
-    return 'index page88888'
+    session["name"] = "itheima"
+    return 'index33333'
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+    app.run()

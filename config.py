1ab39849cd3415eac010ef0bc36d63d0c818582a
niuniu <worldwildwu@outlook.com>
2018-10-25 22:24:49 +0800

    抽取项目的配置到单独的config文件

---
+++ config.py
@@ -0,0 +1,27 @@
+from redis import StrictRedis
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
+
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Redis的配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
--- manage.py
+++ manage.py
@@ -6,33 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
-
-class Config(object):
-    """项目的配置"""
-    DEBUG = True
-
-    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
-
-    # 为数据库添加配置
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Redis的配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # Session保存配置
-    SESSION_TYPE = "redis"
-    # 开启session签名
-    SESSION_USE_SIGNER = True
-    # 指定 Session 保存的 redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 设置需要过期
-    SESSION_PERMANENT = False
-    # 设置过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-
+from config import Config
 
 app = Flask(__name__)
 # 加载配置
@@ -57,7 +31,7 @@ manager.add_command('db', MigrateCommand)
 @app.route('/')
 def index():
     session["name"] = "itheima"
-    return 'index33333'
+    return 'index now'
 
 
 if __name__ == '__main__':

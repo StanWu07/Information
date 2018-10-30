5fd9143b9fb7c9c491d0812fd0028a129b7681cb
StanWu07 <worldwildwu@outlook.com>
2018-10-30 19:43:20 +0800

    首页新闻列表数据的后端实现

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config(object):
     # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 在请求结束时候，如果指定此配置为 True ，那么 SQLAlchemy 会自动执行一次 db.session.commit()操作
     SQLALCHEMY_COMMIT_TEARDOWN = True
     # Redis的配置
     REDIS_HOST = "127.0.0.1"

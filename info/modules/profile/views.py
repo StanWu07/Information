08e4bd09184265e33e01a38bd84a1fc9658f38ef
StanWu07 <worldwildwu@outlook.com>
2018-11-02 20:14:02 +0800

    上传头像的准备&工具准备

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,10 +1,18 @@
 from flask import g
-from flask import redirect
+from flask import redirect, jsonify
 from flask import render_template
 from flask import request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@profile_blu.route('/pic_info', methods=["POST", "GET"])
+@user_login_data
+def pic_info():
+    if request.method == "GET":
+        return render_template('news/user_pic_info.html', data={"user": g.user.to_dict()})
 
 
 @profile_blu.route('/base_info', methods=["GET", "POST"])
@@ -15,6 +23,23 @@ def base_info():
         return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
 
     # 代表是修改用户数据
+    # 1.取到传入的参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 2.校验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if gender not in ("MAN", "WOMAN"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    # 创建用户模型
+    user = g.user
+    user.nick_name = nick_name
+    user.signature = signature
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profile_blu.route('/info')

85c30f5cf13caa92559a34a9275ac208bea1b550
StanWu07 <worldwildwu@outlook.com>
2018-11-02 13:57:08 +0800

    基本资料界面展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,9 +1,19 @@
 from flask import g
 from flask import redirect
 from flask import render_template
+from flask import request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+
+
+@profile_blu.route('/base_info', methods = ["POST","GET"])
+@user_login_data
+def base_info():
+    # 不同的请求方式做不同的事情
+    if request.method == "GET":
+        return render_template("news/user_base_info.html", data={"user": g.user.to_dict()})
+    # 代表是修改用户数据
 
 
 @profile_blu.route('/info')

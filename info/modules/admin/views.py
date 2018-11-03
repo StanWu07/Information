d7239aa5bc8d14d1062b8a59b79ccb400d74104d
StanWu07 <worldwildwu@outlook.com>
2018-11-03 21:31:43 +0800

    总人数/月新增数/日新增数

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,3 +1,5 @@
+import time
+from datetime import datetime
 from flask import current_app
 from flask import redirect
 from flask import render_template
@@ -9,6 +11,39 @@ from flask import g
 from info.models import User
 from info.modules.admin import admin_blu
 from info.utils.common import user_login_data
+
+
+@admin_blu.route('/user_count')
+def user_count():
+    # 总人数
+    total_count = 0
+    try:
+        total_count = User.query.filter(User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 月新增数
+    mon_count = 0
+    t = time.localtime()
+    begin_mon_date = datetime.strptime(('%d-%02d-01' % (t.tm_year, t.tm_mon)), "%Y-%m-%d")
+    try:
+        mon_count = User.query.filter(User.is_admin == False, User.create_time > begin_mon_date).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 日新增数
+    day_count = 0
+    begin_day_date = datetime.strptime(('%d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)), "%Y-%m-%d")
+    try:
+        day_count = User.query.filter(User.is_admin == False, User.create_time > begin_day_date).count()
+    except Exception as e:
+        current_app.logger.error(e)
+    data = {
+        "total_count": total_count,
+        "mon_count": mon_count,
+        "day_count": day_count
+    }
+    return render_template('admin/user_count.html', data=data)
 
 
 @admin_blu.route('/index')

6a03f19a9d3e9792eb42206bed978183112e3437
StanWu07 <worldwildwu@outlook.com>
2018-11-03 19:42:30 +0800

    集成创建超级管理员的的函数到命令行

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ from info import create_app, db, models
 
 # 通过指定的配置名字创建对应配置的app
 # create_app 就类似于工厂方法
+from info.models import User
+
 app = create_app('development')
 
 manager = Manager(app)
@@ -14,6 +16,30 @@ manager = Manager(app)
 Migrate(app, db)
 # 将迁移命令添加到manager中
 manager.add_command('db', MigrateCommand)
+
+
+@manager.option('-n', '-name', dest='name')
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+
+    if not all([name, password]):
+        print("参数不足")
+
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+    print("添加成功超级管理员")
+
 
 if __name__ == '__main__':
     print(app.url_map)

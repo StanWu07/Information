31501a93cb97db3a58377c8adb9d2c9c7c5824ed
StanWu07 <worldwildwu@outlook.com>
2018-11-04 09:54:26 +0800

    待审核的新闻列表数据显示

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -8,9 +8,41 @@ from flask import session
 from flask import url_for
 from flask import g
 from info import constants
-from info.models import User
+from info.models import User, News
 from info.modules.admin import admin_blu
 from info.utils.common import user_login_data
+
+
+@admin_blu.route('news_review')
+def news_review():
+    page = request.args.get("p", 1)
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = News.query.filter(News.status!=0).order_by(News.create_time.desc()).paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_list = []
+    for news in news_list:
+        news_dict_list.append(news.to_review_dict())
+
+    context = {"total_page": total_page, "current_page": current_page, "news_list": news_dict_list}
+
+    return render_template('admin/news_review.html', data=context)
 
 
 @admin_blu.route('/user_list')
